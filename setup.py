53aa35aee0d1e140ae8f6248d52bc672c1990a5d
anrkpk <anr.kpk@gmail.com>
2025-05-17 21:36:18 -0500

    removed unused files

--- setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import find_packages, setup
-
-setup (
-    name='Tesla RAG GenAI Project',
-    Version = '0.0.0',
-    author = 'Narasi',
-    packages= find_packages(),
-    install_requires = []
-)
